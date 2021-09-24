d16035c30c1e358a820d16260d6c1caf5e8e18e0
Andres Prieto <afprietoa@unal.edu.co>
2021-09-24 17:47:33 -0500

    STO-AG

--- data/data.js
+++ data/data.js
@@ -4,72 +4,72 @@ export const data =
       "precio": 500,
       "id": 1,
       "name": "Producto 1",
-      "image": "/images/product1.jpg"
+      "image": "https://raw.githubusercontent.com/wpcodevo/ecom5/master/images/product1.jpg"
     },
     {
       "precio": 650,
       "id": 2,
       "name": "Producto 2",
-      "image": "/images/product2.jpg"
+      "image": "https://raw.githubusercontent.com/wpcodevo/ecom5/master/images/product2.jpg"
     },
     {
       "precio": 550,
       "id": 3,
       "name": "Producto 3",
-      "image": "/images/product3.jpg"
+      "image": "https://raw.githubusercontent.com/wpcodevo/ecom5/master/images/product3.jpg"
     },
     {
       "precio": 400,
       "id": 4,
       "name": "Producto 4",
-      "image": "/images/product4.jpg"
+      "image": "https://raw.githubusercontent.com/wpcodevo/ecom5/master/images/product4.jpg"
     },
     {
       "precio": 350,
       "id": 5,
       "name": "Producto 5",
-      "image": "/images/product5.jpg"
+      "image": "https://raw.githubusercontent.com/wpcodevo/ecom5/master/images/product5.jpg"
     },
     {
       "precio": 700,
       "id": 6,
       "name": "Producto 6",
-      "image": "/images/product6.jpg"
+      "image": "https://raw.githubusercontent.com/wpcodevo/ecom5/master/images/product6.jpg"
     },
     {
       "precio": 150,
       "id": 7,
       "name": "Producto 7",
-      "image": "/images/product7.jpg"
+      "image": "https://raw.githubusercontent.com/wpcodevo/ecom5/master/images/product8.jpg"
     },
     {
       "precio": 210,
       "id": 8,
       "name": "Producto 8",
-      "image": "/images/product8.jpg"
+      "image": "https://raw.githubusercontent.com/wpcodevo/ecom5/master/images/product9.jpg"
     },
     {
       "precio": 170,
       "id": 9,
       "name": "Producto 9",
-      "image": "/images/product9.jpg"
+      "image": "https://raw.githubusercontent.com/wpcodevo/ecom5/master/images/product10.jpg"
     },
     {
       "precio": 220,
       "id": 10,
       "name": "Producto 10",
-      "image": "/images/product10.jpg"
+      "image": "https://raw.githubusercontent.com/wpcodevo/ecom5/master/images/product11.jpg"
     },
     {
       "precio": 100,
       "id": 11,
       "name": "Producto 11",
-      "image": "/images/product11.jpg"
+      "image": "https://raw.githubusercontent.com/wpcodevo/ecom5/master/images/product12.jpg"
     },
     {
       "precio": 400,
       "id": 12,
       "name": "Producto 12",
-      "image": "/images/product12.jpg"
+      "image": "https://raw.githubusercontent.com/wpcodevo/ecom5/master/images/product13.jpg"
     }
   ]
